d5d911bc5b4fbfbc85ec5c2799aad62dab1e4659
Aaron Belmore <belmoreaaron9@gmail.com>
2023-08-06 21:08:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+server.get("/hello", (req, res) => {
+  res.json("hello, there");
+});
+
+const port = process.env.PORT || 9000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
